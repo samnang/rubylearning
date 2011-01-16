35566971d836aa0f440dad66f9d7ccceea80cd0f
samnang <samnang.chhun@gmail.com>
2011-01-16 08:53:46 +0700

    update metaprogramming ex3

--- metaprogramming/week1/ex3.rb
+++ metaprogramming/week1/ex3.rb
@@ -11,43 +11,106 @@ Exercise 3:
 
 =end
 
-animal = 'dog'
+def test_singleton_method
+  animal = 'dog'
 
-def animal.speak
-  'Dog says Woof!'
+  yield animal
+
+  puts animal.speak
 end
 
-class << animal
-  def speak
-    'Dog says Woof!'
+#=============================================
+
+test_singleton_method do |dog|
+  def dog.speak
+    "Woof!"
   end
 end
 
-class << animal
-  define_method :speak do
-    'Dog says Woof!'
+test_singleton_method do |dog|
+  dog.instance_eval do
+    def speak
+      "Woof!"
+    end
   end
 end
 
-animal.instance_eval do
-  def speak
-    'Dog says Woof!'
+test_singleton_method do |dog|
+  class << dog
+    def speak
+      "Woof!"
+    end
   end
 end
 
-class << animal
-  send :define_method, :speak, lambda { puts 'Dog says Woof!' }
-end
-
-module Speakable
-  def speak
-    'Dog says Woof!'
+test_singleton_method do |dog|
+  class << dog
+    define_method(:speak) do
+      "Woof!"
+    end
   end
 end
 
-class << animal
-  include Speakable
+test_singleton_method do |dog|
+  class << dog
+    define_method(:speak, lambda { "Woof!" })
+  end
 end
 
-animal.extend Speakable
+test_singleton_method do |dog|
+  module SpeakableDog
+    def speak
+      "Woof!"
+    end
+  end
 
+  class << dog
+    include SpeakableDog
+  end
+end
+
+test_singleton_method do |dog|
+  module SpeakableDog
+    def speak
+      "Woof!"
+    end
+  end
+
+  dog.extend SpeakableDog
+end
+
+#================ Ruby 1.9.2 ====================
+
+test_singleton_method do |dog|
+  module SpeakableDog
+    def speak
+      "Woof!"
+    end
+  end
+
+  dog.singleton_class.send(:include, SpeakableDog)
+end
+
+test_singleton_method do |dog|
+  dog.singleton_class.class_eval do
+    def speak
+      "Woof!"
+    end
+  end
+end
+
+test_singleton_method do |dog|
+  dog.singleton_class.send(:define_method, :speak) do
+    "Woof!"
+  end
+end
+
+test_singleton_method do |dog|
+  dog.define_singleton_method(:speak) do
+    "Woof!"
+  end
+end
+
+test_singleton_method do |dog|
+  dog.singleton_class.send(:define_method, :speak, lambda { "Woof!" })
+end
